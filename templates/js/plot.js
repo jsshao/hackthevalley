3bd4d5116d8ceb11ef504aa460b7915cb000ba4e
Matthew Tse <mtse9779@gmail.com>
2017-01-08 07:20:20 -0500

    Colors and fine tuning UI

--- templates/js/plot.js
+++ templates/js/plot.js
@@ -1,40 +1,41 @@
-// generate random x/y data
-let randomXY = function () {
-    let x = []
-    let y = []
-    let prev = 0.1;
-    for (let i = 0; i < 157; i += 3) {
-        x.push(i);
-        let randomY;
-        if (Math.round(Math.random())) {
-            randomY = prev + Math.random()
-        } else {
-            randomY = prev - Math.random() * prev
-        }
-        y.push(randomY);
-        prev = randomY
-    }
-    yMax = Math.max(...y)
-    for (let i = 0, length = y.length; i < length; i++) {
-        y[i] = y[i] / yMax;
-    }
-    return [x, y]
-}
-let gData1 = []
-for (let i = 0; i < 4; i++) {
-    let xy = randomXY();
-    let datapoint = {
-        x: xy[0],
-        y: xy[1],
-        type: 'scatter',
-        name: 'random' + i
-    }
-    gData1.push(datapoint)
-}
+// // generate random x/y data
+// let randomXY = function () {
+//     let x = []
+//     let y = []
+//     let prev = 0.1;
+//     for (let i = 0; i < 157; i += 3) {
+//         x.push(i);
+//         let randomY;
+//         if (Math.round(Math.random())) {
+//             randomY = prev + Math.random()
+//         } else {
+//             randomY = prev - Math.random() * prev
+//         }
+//         y.push(randomY);
+//         prev = randomY
+//     }
+//     yMax = Math.max(...y)
+//     for (let i = 0, length = y.length; i < length; i++) {
+//         y[i] = y[i] / yMax;
+//     }
+//     return [x, y]
+// }
+// let gData1 = []
+// for (let i = 0; i < 4; i++) {
+//     let xy = randomXY();
+//     let datapoint = {
+//         x: xy[0],
+//         y: xy[1],
+//         type: 'scatter',
+//         name: 'random' + i
+//     }
+//     gData1.push(datapoint)
+// }
 
 let gData = []
 let vidId = VIDEO_ID;
 let rawGData;
+let colors = ['rgb(233, 69, 52)','rgb(233, 172, 52)','rgb(71, 36, 60)','rgb(126, 209, 47)','rgb(94, 66, 47)','rgb(57, 55, 163)','rgb(222, 198, 35)']
 $.ajax("https://23.101.131.211/metric",{
   data: JSON.stringify({"video_id":vidId}),
   contentType: 'application/json',
@@ -45,6 +46,7 @@ $.ajax("https://23.101.131.211/metric",{
 }).done(function(){
   emotions = Object.keys(rawGData[0]);
   emotions.splice(emotions.indexOf("timestamp"),1);
+  emotions.splice(emotions.indexOf("contempt"),1);
 
   for (let j=0; j < emotions.length; j++){
     x = []
@@ -57,23 +59,18 @@ $.ajax("https://23.101.131.211/metric",{
       x,
       y,
       type: 'scatter',
-      name: emotions[j]
+      name: emotions[j],
+      marker: {
+        color: colors[j]
+      }
     }
     gData.push(datapoint)
   }
-  // for (let i = 0; i < emotions.length; i++) {
-  //
-  // }
   layout = {
       hovermode: 'closest',
       title: 'Click to fastforward video to current time'
   };
   Plotly.newPlot('graph', gData, layout);
-  // enable click to skip to point in video
-  let graph = document.getElementById('graph')
-  graph.on('plotly_click', function (data) {
-    player.seekTo(data.points[0].x, true);
-  });
 })
 
 let rawData;
@@ -90,12 +87,18 @@ $.ajax("https://23.101.131.211/demographic",{
       y: ["13-17","18-24","25-35","35-44","45-54","55+"],
       x: rawData.age,
       type: 'bar',
-      orientation: 'h'
+      orientation: 'h',
+      marker:{
+        color: 'rgb(144, 168, 205)'
+      }
   }]
   let bData = [
     {
       labels: ["male","female"],
       values: rawData.gender,
+      marker:{
+        colors: ['rgb(144, 168, 205)','rgb(247, 203, 202)']
+      },
       type: 'pie'
   }]
 
