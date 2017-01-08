e6185b1c70496bd71e9a20c4f7d1131410dd730b
Kevin Yang <k46yang@uwaterloo.ca>
2017-01-08 06:01:57 -0500

    Added ui flow

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from flask import Flask, jsonify, request, render_template, send_from_directory,
 from flask_cors import CORS, cross_origin
 from cognitive import emotions, face
 from db import insertMetric, insertUser, userExists, getAllVideoIds, getVideoMetrics, getDemographic
+from apiclient.discovery import build
+
+DEVELOPER_KEY = "AIzaSyAcNZjvkEVgF7b8l4Mwc5xUu-7r2yRh8m4"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
 
 app = Flask(__name__)
 CORS(app)
@@ -49,6 +54,7 @@ def metric():
         abort(400)
     return jsonify(getVideoMetrics(request.json['video_id']))
 
+
 @app.route('/demographic', methods=['POST'])
 @cross_origin()
 def demographic():
@@ -56,15 +62,18 @@ def demographic():
         abort(400)
     return jsonify(getDemographic(request.json['video_id']))
 
+
 @app.route('/all_videos', methods=['GET'])
 @cross_origin()
 def all_videos():
     return jsonify(getAllVideoIds())
 
+
 @app.route('/admin/<string:video_id>', methods=['GET'])
 @cross_origin()
 def admin(video_id):
     return render_template("admin.html", video_id=video_id)
+
 
 @app.route('/video/<string:video_id>', methods=['GET'])
 @cross_origin()
@@ -76,7 +85,47 @@ def video(video_id):
 @cross_origin()
 def select():
     video_ids = getAllVideoIds()
-    return render_template("select.html", video_ids=video_ids)
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+    videos = []
+    for video_id in video_ids:
+        search_response = youtube.videos().list(
+            id=video_id,
+            part="snippet",
+            maxResults=1
+        ).execute()
+
+        for search_result in search_response.get("items", []):
+            video_title = search_result["snippet"]["title"]
+            videos.append((video_id, video_title))
+    return render_template("select.html", videos=videos)
+
+
+@app.route('/search', methods=['GET'])
+@cross_origin()
+def search():
+    return render_template("search.html")
+
+
+@app.route('/search_results', methods=['POST'])
+@cross_origin()
+def search_results():
+    query_string = request.form.get("query_string", "")
+
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+    search_response = youtube.search().list(
+        q=query_string,
+        part="id,snippet",
+        maxResults=25
+    ).execute()
+    videos = []
+    for search_result in search_response.get("items", []):
+        if "videoId" in search_result["id"]:
+            video_id = search_result["id"]["videoId"]
+            video_title = search_result["snippet"]["title"]
+            videos.append((video_id, video_title))
+
+    return render_template("search_results.html", videos=videos)
+
 
 @app.route('/js/<path:path>')
 @cross_origin()
